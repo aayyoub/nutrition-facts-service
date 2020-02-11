d429df5ec533eee4d016fee472f0464633c34cb0
Ala Ayyoub <aayyoub@expedia.com>
2020-02-11 02:14:14 +0200

    Finish Redis cache.

--- src/main/java/com/food/information/service/configuration/CacheConfiguration.java
+++ src/main/java/com/food/information/service/configuration/CacheConfiguration.java
@@ -3,14 +3,29 @@ package com.food.information.service.configuration;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.RedisSerializer;
+
+import java.time.Duration;
 
 @Configuration
 @EnableCaching
 public class CacheConfiguration {
     @Bean
-    public RedisSerializer<Object> redisSerializer() {
-        return new Jackson2JsonRedisSerializer(Object.class);
+    public RedisCacheManager redisCacheManager(LettuceConnectionFactory lettuceConnectionFactory) {
+        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration
+                .defaultCacheConfig()
+                .disableCachingNullValues()
+                .entryTtl(Duration.ofHours(24))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(RedisSerializer.json()));
+        redisCacheConfiguration.usePrefix();
+
+        return RedisCacheManager.RedisCacheManagerBuilder
+                .fromConnectionFactory(lettuceConnectionFactory)
+                .cacheDefaults(redisCacheConfiguration)
+                .build();
     }
 }
