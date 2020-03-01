4e637b96fa79693ee73b53fe79fff469ec4b5115
Ala Ayyoub <aayyoub@expedia.com>
2020-03-01 22:11:20 +0200

    Formatting for explore foods.

--- src/main/java/com/food/information/service/domain/formatter/ExploreFoodFormatter.java
+++ src/main/java/com/food/information/service/domain/formatter/ExploreFoodFormatter.java
@@ -2,6 +2,7 @@ package com.food.information.service.domain.formatter;
 
 import com.food.information.service.domain.model.ExploreFood;
 import com.food.information.service.domain.seo.SeoUrlBuilder;
+import org.apache.commons.text.WordUtils;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -15,7 +16,11 @@ public class ExploreFoodFormatter {
     }
 
     public List<ExploreFood> formatFood(List<ExploreFood> exploreFoods) {
-        exploreFoods.forEach(exploreFood -> exploreFood.setUrl(seoUrlBuilder.buildUrl(exploreFood.getFoodId(), exploreFood.getFoodDescription())));
+        exploreFoods.forEach(exploreFood -> {
+                    exploreFood.setUrl(seoUrlBuilder.buildUrl(exploreFood.getFoodId(), exploreFood.getFoodDescription()));
+                    exploreFood.setFoodDescription(WordUtils.capitalizeFully(exploreFood.getFoodDescription()));
+                }
+        );
 
         return exploreFoods;
     }
