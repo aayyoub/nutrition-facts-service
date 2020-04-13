d06394bf1687b9ed87d9b740b9873da45db49436
Ala Ayyoub <aayyoub@expedia.com>
2020-04-14 01:48:35 +0300

    Fix issue with cache.

--- src/main/java/com/food/information/service/domain/model/NutritionalStatement.java
+++ src/main/java/com/food/information/service/domain/model/NutritionalStatement.java
@@ -1,14 +1,17 @@
 package com.food.information.service.domain.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @EqualsAndHashCode
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class NutritionalStatement {
     boolean isBeneficial;
