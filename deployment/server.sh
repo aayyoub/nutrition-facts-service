900d1dfd309d2ee2a038ee48d136ff2785e96386
Ala Ayyoub <aayyoub@expedia.com>
2020-05-05 20:03:13 +0300

    Update environment file name.

--- deployment/server.sh
+++ deployment/server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ -f ~/.bash_aliases ]; then
-  . ~/.bash_aliases
+if [ -f ~/.environment ]; then
+    . ~/.environment
 fi
 
 java -jar -Dspring.profiles.active=prod -Dspring.datasource.url=$DB_URL -Dspring.datasource.username=$DB_USERNAME -Dspring.datasource.password=$DB_PASSWORD /applications/food-information-service/target/food-information-service.jar
