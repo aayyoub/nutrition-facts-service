2109a5adbddf817d4cea0889c4374882fd37a6df
Ala Ayyoub <aayyoub@expedia.com>
2020-05-05 19:29:22 +0300

    Update server script.

--- deployment/server.sh
+++ deployment/server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -jar -Dspring.profiles.active=prod /applications/food-information-service/target/food-information-service.jar
+java -jar -Dspring.profiles.active=prod -Dspring.datasource.url=$DB_URL -Dspring.datasource.username=$DB_USERNAME -Dspring.datasource.password=$DB_PASSWORD /applications/food-information-service/target/food-information-service.jar
