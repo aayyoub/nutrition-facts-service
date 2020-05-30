a5d7c332aab30bc823e412147c039cd1a35be623
Ala Ayyoub <aayyoub@expedia.com>
2020-05-30 18:06:54 +0300

    Fix nutrient group bug.

--- src/main/java/io/nutritionfacts/api/domain/builder/nutrientgroup/grouper/EnergyGrouper.java
+++ src/main/java/io/nutritionfacts/api/domain/builder/nutrientgroup/grouper/EnergyGrouper.java
@@ -2,10 +2,12 @@ package io.nutritionfacts.api.domain.builder.nutrientgroup.grouper;
 
 import io.nutritionfacts.api.domain.builder.nutrientgroup.AbstractNutrientGrouper;
 import io.nutritionfacts.api.domain.util.NutrientId;
+import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
 import java.util.List;
 
+@Component
 public class EnergyGrouper extends AbstractNutrientGrouper {
     private static final Integer RANK = 0;
     private static final String GROUP_NAME = "Energy";
